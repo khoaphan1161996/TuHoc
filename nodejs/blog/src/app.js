eb4c5e7ed62d9ee10d4ef0c01cffeba3bc0d2382
khoaphan1161996 <khoa071996@gmail.com>
2021-04-15 10:46:03 +0700

    1/2nodejs

--- nodejs/blog/src/app.js
+++ nodejs/blog/src/app.js
@@ -7,6 +7,10 @@ const port = 3000
 
 // cách cấu hình static file
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({   // import middleware body để xử lý submit form
+  extended: true
+}))  
+app.use(express.json())   // import middleware body để xử lý submit form khi dùng thư viện js (XMLHttpRequest, fetch, axios, jqueryajax)
 
 // template engine
 app.engine('hbs', handlebars({
@@ -21,7 +25,18 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+  console.log(req.query.q) 
   res.render('news')
+})
+
+app.get('/search', (req, res) => {
+  console.log(req.query)  // lấy key query trong req
+  res.render('search')
+})
+
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('')
 })
 
 app.listen(port, () => {
