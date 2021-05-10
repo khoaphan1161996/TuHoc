7e919396ea761f322d5859ae888272b577c67e84
khoaphan1161996 <khoa071996@gmail.com>
2021-05-10 21:33:21 +0700

    a

--- ECMAScript 6+/script.js
+++ ECMAScript 6+/script.js
@@ -5,7 +5,7 @@
 // const sum = (a, b) => ({a: a, b: b}) // Khi muốn return ra object ta phải gói trong cặp ()
 // console.log(sum(2, 2))               // Khi truyền vào 1 tham số có thể bỏ ()
 
-// Video 100: Class: cách viết khác của constructor function
+// Video 102: Class: cách viết khác của constructor function
 // function Course(name,price) {
 //         this.name = name;
 //         this.price = price;
@@ -31,7 +31,7 @@
 // console.log(phpCourse)
 // console.log(jsCourse)
 
-// Video 103: Destructuring
+// Video 105: Destructuring
 // var course = {
 //     name: 'javascript',
 //     price: 1000,
@@ -45,3 +45,15 @@
 // console.log(parentName)
 // console.log(childrenName)
 // console.log(description)
+
+// Video 106: spread: dùng spread sẽ làm mất đi cặp ngoặc trước và sau toán tử
+// var array1 = [1,2,3]
+// var array2 = [4,5,6]
+// var array = [...array1,...array2]
+// console.log(array)
+
+// var obj1 = {name:'js'}
+// var obj2 = {price:1000}
+// var obj3 = {...obj1,...obj2}
+// console.log(obj3)
+
