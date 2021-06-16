d9b264ea6d039b7f91ead9bde8e1de59ae9cc900
khoa071996@gmail.com <khoaphan1161996>
2021-06-16 23:57:13 +0700

    fixloi

--- project2/src/components/Sort/index.js
+++ project2/src/components/Sort/index.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 function Sort() {
   return (
     <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
--- project2/src/components/TaskItem/index.js
+++ project2/src/components/TaskItem/index.js
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 function TaskItem(props) {
   const {index,task} = props;
   const {id,name,status} = task
