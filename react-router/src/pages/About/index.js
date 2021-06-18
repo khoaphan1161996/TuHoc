c1ee4cce9adff67b8131568f6be038f961f7e835
khoa071996@gmail.com <khoaphan1161996>
2021-06-18 11:04:51 +0700

    initital

---
+++ react-router/src/pages/About/index.js
@@ -0,0 +1,12 @@
+
+
+function About(props) {
+
+    return (
+        <div>
+            Đây là trang về chúng tôi
+        </div>
+    )
+}
+
+export default About
---
+++ react-router/src/pages/HomePage/index.js
@@ -0,0 +1,12 @@
+
+
+function HomePage(props) {
+
+    return (
+        <div>
+            Đây là trang chủ
+        </div>
+    )
+}
+
+export default HomePage
