80e428c082e46cdb1c6a7e5af2c2b6b999dace40
khoaphan1161996 <khoa071996@gmail.com>
2021-07-08 21:23:17 +0700

    frist

---
+++ 30daysJS/02_Day_Data_types.js
@@ -0,0 +1,12 @@
+let challenge = '30 Days Of JavaScript'
+
+// console.log(challenge)
+// console.log(challenge.length)
+// console.log(challenge.toUpperCase())
+// console.log(challenge.toLowerCase())
+// console.log(challenge.substring(0,2))
+// console.log(challenge.slice(3))
+// console.log(challenge.includes('Script'))
+// console.log(challenge.split(''))
+console.log(challenge.split(' '))
+
