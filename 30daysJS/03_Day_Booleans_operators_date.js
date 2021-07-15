2f029d27bf40c91d1d4970ff88587e461cbcfce7
khoaphan1161996 <khoa071996@gmail.com>
2021-07-15 19:06:40 +0700

    1/2ex

---
+++ 30daysJS/03_Day_Booleans_operators_date.js
@@ -0,0 +1,90 @@
+// Exercises: Level 1
+//1.
+let firstName='Khoa';
+let lastName='Phan';
+let city='HCM';
+let country='VietNam';
+let age=25
+let maritalStatus='marry';
+let year = 2021
+// console.log(typeof firstName)
+// console.log(typeof lastName)
+// console.log(typeof city)
+// console.log(typeof country)
+// console.log(typeof age)
+// console.log(typeof maritalStatus)
+// console.log(typeof year)
+
+//2. console.log(typeof '10' === typeof 10)
+//3. console.log(parseInt('9.8') === parseInt(10))
+//4. 
+// console.log(Boolean(1))
+// console.log(Boolean('khoa'))
+// console.log(Boolean(true))
+// console.log(Boolean(false))
+// console.log(Boolean(0))
+// console.log(Boolean(''))
+
+//5. 
+// console.log(4 > 3)
+// console.log(4 >= 3)
+// console.log(4 < 3)
+// console.log(4 <= 3)
+// console.log(4 == 4)
+// console.log(4 === 4)
+// console.log(4 != 4)
+// console.log(4 !== 4)
+// console.log(4 != '4')
+// console.log(4 == '4')
+// console.log(4 === '4')
+// console.log('python'.length > 'jargon'.length)
+
+//6. 
+// console.log(4 > 3 && 10 < 12)
+// console.log(4 > 3 && 10 > 12)
+// console.log(4 > 3 || 10 < 12)
+// console.log(4 > 3 || 10 > 12)
+// console.log(!(4 > 3))
+// console.log(!(4 < 3))
+// console.log(!(false))
+// console.log(!(4 > 3 && 10 < 12))
+// console.log(!(4 > 3 && 10 > 12))
+// console.log(!(4 === '4'))
+
+//7. 
+// const date = new Date()
+// console.log(date.getFullYear())
+// console.log(date.getMonth())
+// console.log(date.getDate())
+// console.log(date.getDay())
+// console.log(date.getHours())
+// console.log(date.getMinutes())
+// console.log(date.getTime())
+
+// Exercises: Level 2
+//1. 
+// let base = Number(prompt('Nhập base của tam giác'))
+// let height = Number(prompt('Nhập height của tam giác'))
+// console.log(`Diện tích tam giác là: ${0.5*base*height}`)
+
+//2. 
+// let sideA = Number(prompt('Nhập side A của tam giác'))
+// let sideB = Number(prompt('Nhập side B của tam giác'))
+// let sideC = Number(prompt('Nhập side C của tam giác'))
+// console.log(`Chu vi tam giác là: ${sideA+sideB+sideC}`)
+
+//3. 
+// let width  = Number(prompt('Nhập width của hình chữ nhật'))
+// let length  = Number(prompt('Nhập length  của hình chữ nhật'))
+// console.log(`Diện tích hình chữ nhật là: ${width*length}`)
+// console.log(`Chu vi hình chữ nhật là: ${2*(width+length)}`)
+
+//4. 
+// let radius  = Number(prompt('Nhập radius của hình tròn'))
+// console.log(`Diện tích hình tròn là: ${3.14*radius*radius}`)
+// console.log(`Chu vi hình tròn là: ${2*3.14*radius}`)
+
+//9. 
+// let enterHours  = Number(prompt('Nhập Enter hours'))
+// let enterRatePerHours  = Number(prompt('Nhập Enter rate per hour'))
+// console.log(`Your weekly earning is ${enterHours*enterRatePerHours}`)
