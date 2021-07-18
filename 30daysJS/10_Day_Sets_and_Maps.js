ff1bc127c862e9b94b6c3497428a0406ce3061e1
khoaphan1161996 <khoa071996@gmail.com>
2021-07-18 11:39:09 +0700

    finishDay10

---
+++ 30daysJS/10_Day_Sets_and_Maps.js
@@ -0,0 +1,39 @@
+// Exercises:Level 1
+//1. 
+// const newSet = new Set();
+//2. 
+// for(let i = 0; i <=10 ;i++) {
+//     newSet.add(i)
+// }
+//3. 
+// newSet.delete(10)
+//4. 
+// newSet.clear()
+
+//5. 
+// const countriesMap = new Map();
+// countriesMap.set(`Finland`,3)
+// countriesMap.set(`Sweden`,4)
+// countriesMap.set(`Norway`,5)
+// console.log(countriesMap)
+
+
+// Exercises:Level 2
+// const a = [4,5,8,9]
+// const b = [3,4,5,7]
+// const countries = ['Finland', 'Sweden', 'Norway']
+//1. 
+// let c = [...a , ...b]
+// let C = new Set(c)
+// console.log(C)
+//2. 
+// let A = new Set(a)
+// let B = new Set(b)
+// let d = b.filter(num => A.has(num))
+// console.log(d)
+//3. 
+// let e = a.filter(num => !B.has(num))
+// console.log(e)
+
+
+// Exercises:Level 3
