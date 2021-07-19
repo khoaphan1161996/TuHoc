d384a53404ca4ec766686be95dce45bcfe0124f2
khoa071996@gmail.com <khoaphan1161996>
2021-07-19 20:19:51 +0700

    okff

--- cart-redux/src/components/Products.js
+++ cart-redux/src/components/Products.js
@@ -1,12 +1,24 @@
 import Product from './Product'
+import {useSelector} from 'react-redux'
 
 function Products() {
+  const products = useSelector(state => state.products)
+
+  const showProduct = (products) => {
+    let result = null
+    if(products.length > 0) {
+      result = products.map(product => 
+        <Product key={product.id} product={product}/>)
+    }
+
+    return result 
+  }
+
   return (
     <section className="section">
         <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
         <div className="row">
-            <Product />
-        
+            {showProduct(products)}  
         </div>
     </section>
   )
