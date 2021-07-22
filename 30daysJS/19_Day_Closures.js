fa4e1e71fb43b63542b5cd98c1f66a10e07bf39a
khoaphan1161996 <khoa071996@gmail.com>
2021-07-22 15:47:17 +0700

    finishDay19

---
+++ 30daysJS/19_Day_Closures.js
@@ -0,0 +1,40 @@
+// Exercises: Level 1
+// function plus() {
+//     let count = 0
+//     function plus1 () {
+//         count += 1
+//         return count
+//     }
+//     return plus1
+// }
+
+// const resultPlus = plus()
+// console.log(resultPlus())
+// console.log(resultPlus())
+// console.log(resultPlus())
+
+
+// Exercises: Level 2
+// function plus() {
+//     let count = 0
+//     function plus1 () {
+//         count += 1
+//         return count
+//     }
+//     function plus2 () {
+//         count += 1
+//         return count
+//     }
+//     function plus3 () {
+//         count += 1
+//         return count
+//     }
+//     return {
+//         plus1: plus1(),
+//         plus2: plus2(),
+//         plus3: plus3(),
+//     }
+// }
+
+// const resultPlus = plus()
+// console.log(resultPlus)
