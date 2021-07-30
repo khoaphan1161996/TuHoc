9a3e0e701e017006da4d0bb928d11022a0611959
khoaphan1161996 <khoa071996@gmail.com>
2021-07-30 23:16:57 +0700

    tichhopRedux

--- learning-redux-saga-material-ui-redux-form/src/App/index.js
+++ learning-redux-saga-material-ui-redux-form/src/App/index.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/core/styles'
-import styles from './styles'
-import theme  from '../commons/Theme';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/core/styles";
+import styles from "./styles";
+import theme from "../commons/Theme";
+import { Provider } from "react-redux";
+import configureStore from "../redux/configureStore";
 
-import Taskboard from '../Taskboard';
+import Taskboard from "../Taskboard";
 
+const store = configureStore()
 
 function App(props) {
-  
   return (
-    <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
         <Taskboard />
-    </ThemeProvider>
+      </ThemeProvider>
+    </Provider>
   );
 }
 
