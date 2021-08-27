cba6df7cdc88e96294586dcc04053c7a318b3106
khoaphan1161996 <khoa071996@gmail.com>
2021-08-27 18:56:36 +0700

    Finishvideo9

---
+++ nodejsCoderX/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const userRoute = require('./routes/user.route')
+
+// config những gì liên quan tới app
+const app = express();
+const port = 8080
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+// config routes
+app.use('/users',userRoute)
+
+app.get('/', (req, res) => {
+    res.render('index', {name: 'Khoa'})
+})
+
+app.listen(port, () => {
+    console.log('listening on port ',port)
+})
