b25be575b4eb4e3ef8e01b0196e896ac1a14a89c
khoaphan1161996 <khoa071996@gmail.com>
2021-09-19 12:46:52 +0700

    okay

--- nodejsCoderX/index.js
+++ nodejsCoderX/index.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
 
 const userRoute = require('./routes/user.route')
+const authRoute = require('./routes/auth.route')
+
+const authMiddleware = require('./middlewares/auth.middleware')
 
 // config những gì liên quan tới app
 const app = express();
 const port = 8080
 app.set('view engine', 'pug')
 app.set('views', './views')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(express.static('public'))
 
+app.use(cookieParser())
+
 // config routes
-app.use('/users',userRoute)
+app.use('/users', authMiddleware.requireAuth,userRoute)
+app.use('/auth',authRoute)
 
 app.get('/', (req, res) => {
     res.render('index', {name: 'Khoa'})
